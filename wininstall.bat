83ed17e9cd073b17adbdbe13998d1bbdda8d5686
Kevin LOCATI <kevin.locati@gmail.com>
2018-04-12 13:53:04 +0200

    Adds windows installer

---
+++ wininstall.bat
@@ -0,0 +1,6 @@
+@"%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -InputFormat None -ExecutionPolicy Bypass -Command "iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))" && SET "PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin"
+choco install python
+pip install -r server/requirements.txt
+pip install -r agent/requirements.txt
+cd server
+python ares.py initdb
