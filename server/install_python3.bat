8b316352dc8d495c1005f5fa47bbbf4423d9bd74
Kevin LOCATI <kevin.locati@gmail.com>
2018-04-18 23:21:34 +0200

    Adds Python3 installer

--- server/install_python3.bat
+++ server/install_python3.bat
@@ -1,6 +1,3 @@
 @"%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -InputFormat None -ExecutionPolicy Bypass -Command "iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))" && SET "PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin"
+choco feature enable -n allowGlobalConfirmation
 choco install python
-pip install -r server/requirements.txt
-pip install -r agent/requirements.txt
-cd server
-python ares.py initdb
